cf80f437a7edccd4f319ebb123897ca2fb671112
Akhilbisht798 <akhilbisht798@gmail.com>
2024-09-04 15:59:34 +0530

    language support done

--- code/src/component/editor/CodeEditor.tsx
+++ code/src/component/editor/CodeEditor.tsx
@@ -7,12 +7,58 @@ const CodeEditor: FC = () => {
   const { file } = useCurrentFile();
   const { updateFile } = useProjectFiles();
 
+  //TODO: Also send changes to sockekt server.
   function onChangeHandler(change: string | undefined, e: any) {
     const path = file?.path + "/" + file?.name;
     updateFile(path, { content: change });
   }
 
-  console.log(file);
+  function getFileMode() {
+    const name = file?.name;
+    if (name === undefined) {
+      return;
+    }
+
+    const splitName = name.split(".");
+    const extension = splitName[splitName.length - 1];
+    switch (extension) {
+      case "js":
+        return "javascript";
+      case "py":
+        return "python";
+      case "java":
+        return "java";
+      case "xml":
+        return "xml";
+      case "rb":
+        return "ruby";
+      case "sass":
+        return "sass";
+      case "md":
+        return "markdown";
+      case "sql":
+        return "mysql";
+      case "json":
+        return "json";
+      case "html":
+        return "html";
+      case "hbs":
+        return "handlebars";
+      case "handlebars":
+        return "handlebars";
+      case "go":
+        return "go";
+      case "cs":
+        return "csharp";
+      case "litcoffee":
+        return "coffee";
+      case "css":
+        return "css";
+      default:
+        return "";
+    }
+  }
+
   return (
     <>
       <div className=" w-full h-full font-sans ">
@@ -21,7 +67,7 @@ const CodeEditor: FC = () => {
           height="75vh"
           width="75vw"
           theme="vs-dark"
-          defaultLanguage="javascript"
+          language={getFileMode()}
           defaultValue="console.log(`hello world`)"
           onChange={onChangeHandler}
         />
