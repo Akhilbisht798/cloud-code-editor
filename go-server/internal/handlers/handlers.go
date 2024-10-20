5df4e4230a6ce6ec994d8e21a6ab9eb4392a00af
Akhilbisht798 <akhilbisht798@gmail.com>
2024-10-20 18:51:00 +0530

    checking secure

--- go-server/internal/handlers/handlers.go
+++ go-server/internal/handlers/handlers.go
@@ -162,6 +162,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		Path:     "/",
 		MaxAge:   3600,
 		SameSite: http.SameSiteNoneMode,
+		Secure:   true,
 	}
 
 	http.SetCookie(w, &cookie)
